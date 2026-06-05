29c37e3b43120da5f4ed1378e3139c05799606a7
Hatem Shalaby <hatemismail2011@gmail.com>
2026-06-05 21:05:56 +0300

    Delete update_integration_reflectore.py

--- update_integration_reflectore.py
+++
@@ -1,6 +0,0 @@
-from core.hitl_queue import record_reflection, is_automation_suspended
-
-# At the end of reflection, even before storing snapshot:
-if not is_automation_suspended():
-    # apply heuristic, etc.
-    record_reflection(deviation_class=deviation_class, heuristic_applied=heuristic_applied)
